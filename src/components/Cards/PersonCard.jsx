0591d444b06cb3d4550ec0cd79930ae55c706a7d
EffDuBois <arnabkar2003@protonmail.com>
2023-09-24 00:51:16 +0530

    Fixed personcard

--- src/components/Cards/PersonCard.jsx
+++ src/components/Cards/PersonCard.jsx
@@ -1,19 +1,19 @@
 import React from "react";
 
-
 const PersonCard = (props) => {
   return (
     <div className="bg-rotatingx h-[500px] min-w-[420px]  my-10">
       <div className="bg-rotatingx x2">
         <div className="content h-[97%] w-[93%]">
           <img
-            className="h-full cover hover:opacity-5 duration-150 absolute"
+            className="h-full cover hover:opacity-0 duration-150 absolute"
             src={props.src}
             alt={props.name}
           />
-          <div className=" h-full p-10 flex flex-col justify-items-center items-start  ">
-            <p>{props.name}</p>
-            <p>{props.title}</p>
+          <div className=" h-full p-10 flex flex-col justify-center">
+            <p className="font-bold text-3xl ">{props.name}</p>
+            <p className="font-bold">{props.title}</p>
+            <div className="h-3 bg-red-500" />
           </div>
         </div>
       </div>
