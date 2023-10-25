3bde064713cf011c25764b6bd471b291b540fd50
Arnab Kar <46263523+EffDuBois@users.noreply.github.com>
2023-10-25 14:54:09 +0530

    updated the size of people cards

--- src/components/cards/PersonCardDesc.jsx
+++ src/components/cards/PersonCardDesc.jsx
@@ -7,7 +7,7 @@ const PersonCardDesc = (props) => {
       className={`flex lg:block lg:overflow-hidden flex-col items-center lg:items-start lg:flex-row my-8 lg:my-16`}
     >
       <PersonCard
-        className={`lg:h-[34vw] lg:w-[34vw] lg:float-left mb-4 lg:mb-4 lg:mr-6 w-[90vw] h-[85vw] `}
+        className={`lg:h-[40vw] lg:w-[34vw] lg:float-left mb-4 lg:mb-4 lg:mr-6 w-[90vw] h-[85vw] `}
         name={props.name}
         title={props.title}
         contact={props.contact}
