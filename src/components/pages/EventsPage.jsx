6fead16c2754c1a846a084aade605f71c4cd5914
EffDuBois <arnabkar2003@protonmail.com>
2023-11-17 12:39:16 +0530

    optimised the reveal vid

--- src/components/pages/EventsPage.jsx
+++ src/components/pages/EventsPage.jsx
@@ -8,7 +8,7 @@ import PeopleTray from "../trays/PeopleTray";
 import Counter from "../counters/Counter";
 
 import logobg from "../../resources/images/logos/insightx/logo-insightx-nobg.webp";
-import videobg from "../../resources/videos/insightx-revealvid-lowres.mp4"
+import videobg from "../../resources/videos/insightx-scaled-down.mp4"
 
 import sponsorImages from "../../resources/images/sponsors/getSponsorImages";
 import speakerImages from "../../resources/images/speakers/getSpeakerImages";
