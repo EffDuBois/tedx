35838f6f95daef1e0fa677872949efdc9e00a9fe
EffDuBois <arnabkar2003@protonmail.com>
2023-11-20 17:17:15 +0530

    edited titles

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
+
 import App from "./App";
+
 import { BrowserRouter } from "react-router-dom";
+
+import "./index.css";
+
 import("preline");
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
